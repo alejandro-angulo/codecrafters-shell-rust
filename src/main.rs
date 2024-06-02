19e16e4b558ffc0e3e1fe220b93defb7090548bc
Alejandro Angulo <iam@alejandr0angul0.dev>
2024-06-02 16:27:28 -0700

    Added loop

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // Remove trailing newline
-    input.pop();
-    println!("{}: command not found", input);
+        // Remove trailing newline
+        input.pop();
+        println!("{}: command not found", input);
+    }
 }
